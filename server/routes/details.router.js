3b7a6760f3323ed627d3761edc318371f9cfae34
Josh Maus <jman985@gmail.com>
2020-07-26 00:18:41 -0500

    added routes

---
+++ server/routes/details.router.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const { default: Axios } = require('axios');
+const { query } = require('../modules/pool');
+const router = express.Router();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
