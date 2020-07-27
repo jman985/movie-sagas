3a158505cda21ccdbe4ec70d46290948c65be087
Josh Maus <jman985@gmail.com>
2020-07-26 19:26:59 -0500

    added PUT

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -15,7 +15,32 @@ router.get('/', (req, res) => {
 })
 
 
-
+// save the user's changes to movie title and description
+router.put('/', async (req, res) => {   
+    const client = await pool.connect();
+    try {
+        const {
+            title,
+            description,
+            id
+        } = req.body;
+        await client.query('BEGIN');
+        
+        await client.query
+        (`UPDATE movies
+          SET title = $1,
+              description = $2
+          WHERE id = $3;`, [ title, description, id ] );
+        await client.query('COMMIT')
+        res.sendStatus(201);
+    } catch (error) {
+        await client.query('ROLLBACK')
+        console.log('Error POST /api/order', error);
+        res.sendStatus(500);
+    } finally {
+        client.release()
+    }
+});
 
 
 
